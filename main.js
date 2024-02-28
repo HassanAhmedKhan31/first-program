0be5a2d8ff401dab19b0c38a72fa5fcc85003b4c
HassanAhmedKhan31 <161369304+HassanAhmedKhan31@users.noreply.github.com>
2024-02-28 08:01:55 +0500

    new

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("hello world\n");
+console.log("My name is hassan ahmed khan\n");
+console.log("i am learning typescript\n");
+console.log("which is very interesting\n");
+console.log("\t\tApplication\t\t\n");
+console.log("To\n The headmistress,\n Children Model Academy,\n Liaquatabad, \n Karachi. \n");
+console.log("Respected Madam,\n\t Most Respectfuuly, I beg to state that I am the student of class 8 of your school");
---
+++ main.ts
@@ -0,0 +1,8 @@
+console.log("hello world\n");
+console.log("My name is hassan ahmed khan\n");
+console.log("i am learning typescript\n");
+console.log("which is very interesting\n");
+console.log("\t\tApplication\t\t\n");
+console.log("To\n The headmistress,\n Children Model Academy,\n Liaquatabad, \n Karachi. \n");
+console.log("Respected Madam,\n\t Most Respectfuuly, I beg to state that I am the student of class 8 of your school.My father has been transfer from karachi to lahore our whole family is shifting from Karachi to lahore. Kindly grand me leave a schooling certificate. I will be very thankful to you for this act of kindness.");
+console.log("\n\t\t\t Your's Obdiently,\n\t\t\t X.Y.Z \n\t\t\t Date:")
